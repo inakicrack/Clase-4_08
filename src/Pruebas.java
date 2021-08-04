1e72bc714954468bcc6c4033602e463cab288e93
inakicrack <clase.inaki.001@gmail.com>
2021-08-04 10:51:14 +0100

    Añado la clase Pruebas
    Creacion de Pruebas.java y cambios ne Elementodivisible

---
+++ src/Pruebas.java
@@ -0,0 +1,14 @@
+// Aitor: Juan Carlos Caso Alonso
+// Fecha: 4 de Agosto de 2021
+// Para ir ejecutando pruebas y ver como funciona el codigo
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
